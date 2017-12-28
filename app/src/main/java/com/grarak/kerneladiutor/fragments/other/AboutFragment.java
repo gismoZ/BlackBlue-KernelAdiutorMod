bea7afd49e5e5205d8b65a1ca04820d0acf37d5c
gismo <tolis_kon@yahoo.gr>
2017-12-28 23:06:17 +0200

    Add app version in about menu

--- app/src/main/java/com/grarak/kerneladiutor/fragments/other/AboutFragment.java
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/other/AboutFragment.java
@@ -25,6 +25,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.grarak.kerneladiutor.BuildConfig;
 import com.grarak.kerneladiutor.R;
 import com.grarak.kerneladiutor.fragments.BaseFragment;
 import com.grarak.kerneladiutor.fragments.RecyclerViewFragment;
@@ -73,6 +74,16 @@ public class AboutFragment extends RecyclerViewFragment {
     }
 
     private void librariesInit(List<RecyclerViewItem> items) {
+        CardView about = new CardView(getActivity());
+        about.setTitle(getString(R.string.app_version));
+
+        DescriptionView app = new DescriptionView();
+        app.setTitle(getString(R.string.app_name));
+        app.setSummary("v" + BuildConfig.VERSION_NAME);
+
+        about.addItem(app);
+        items.add(about);
+
         CardView cardView = new CardView(getActivity());
         cardView.setTitle(getString(R.string.libraries_used));
 
